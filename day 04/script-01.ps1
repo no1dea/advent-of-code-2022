e15cd5b0a29172255aedae14713dbe35b2caa56f
no1dea <philipp_strasser@yahoo.de>
2022-12-13 21:34:40 +0100

    day 04 finished

--- day 04/script-01.ps1
+++ day 04/script-01.ps1
@@ -1,12 +1,24 @@
 $content = Get-Content "./input.txt"
+#$content = "19-67,67-67"
 
-$content.ForEach({
-        $o = $_ -match '(?<r1low>\d{2})-(?<r1high>\d{2}),(?<r2low>\d{2})-(?<r2high>\d{2})'
-        $pairs = @{
-            r1 = @{low = $Matches.r1low; high = $Matches.r1high }
-            r2 = @{low = $Matches.r2low; high = $Matches.r2high }
-        }
-        if ((($pairs.r1.high)..($pairs.r1.low)) -in (($pairs.r2.high)..($pairs.r2.low)) {
+$count = 0
+$count2 = 0
+foreach ($item in $content ) {
+    [int]$r1low = $item.split(",")[0].split("-")[0]
+    [int]$r1high = $item.split(",")[0].split("-")[1]
+    [int]$r2low = $item.split(",")[1].split("-")[0]
+    [int]$r2high = $item.split(",")[1].split("-")[1]
+    
+    if (($r1low -le $r2low) -and ($r2high -le $r1high)) {
+        $count++
+    }
+    elseif (($r2low -le $r1low) -and ($r1high -le $r2high)) {
+        $count++
+    }
+    if (!(($r1high -lt $r2low) -or ($r2high -lt $r1low))) { 
+        $count2++ 
+    }
+}
 
-        }
-    })
+$count
+$Count2
